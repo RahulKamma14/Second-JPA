409af35e150d71c98d1841a12a705f1a154a5847
RahulKamma <rkamma1@umbc.edu>
2023-08-23 23:05:30 -0400

    basic 1

--- src/main/java/com/example/project/second/SecondApplication.java
+++ src/main/java/com/example/project/second/SecondApplication.java
@@ -8,6 +8,10 @@ public class SecondApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SecondApplication.class, args);
+        double a = 200;
+        double b = 0;
+        b = a%2==0 ? a/4:a*2;//ternary operator
+        System.out.println(b);
     }
 
 }
